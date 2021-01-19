5cf59af643927404821c5c17b90a8a04c771e459
Mahadi Hassan <itsmahadi@gmail.com>
2021-01-19 06:16:35 +0600

    Update to Mysql Database

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -75,10 +75,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+'ENGINE': 'django.db.backends.mysql',
+'NAME': 'polls',
+'USER': 'root',
+'PASSWORD': '',
+'HOST': '',
+'PORT': '',
+'OPTIONS': {
+'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+}
+}
 }
 
 
