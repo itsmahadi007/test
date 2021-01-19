0e2eca5eb6a20998a6acb73ddc14f8fe00d2310d
Mahadi Hassan <itsmahadi@gmail.com>
2021-01-19 19:21:45 +0600

    update view student_show

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -6,3 +6,10 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def student_show(request):
+    x=[]
+    for i in range(10):
+        x.append(i)
+    return HttpResponse("<h1>Mahadi Hassan Django Tutorials</h1> The digits are {0}".format(x))
+    
