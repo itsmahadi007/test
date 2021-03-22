bfd4f5c2a4887ae9b2c37aa46fdf3b4a39f53e57
Mahadi Hassan <itsmahadi@gmail.com>
2021-03-22 16:58:50 +0600

    upde new win env

--- win_env/Scripts/django-admin.py
+++ win_env/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!C:\Users\itsma\Documents\Playing in Github\Simple-Django-Project\win_env\Scripts\python.exe
+#!C:\Users\Mahadi\Documents\Playground\Simple-Django-Project\win_env\Scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
